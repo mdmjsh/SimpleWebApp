7c999de49c8ff74b7dba20b1084a86366181ae9e
mdmjsh <joshharrison13@gmail.com>
2020-02-17 14:59:17 +0000

    Revert "this will break! updated hardcoded query term"
    This reverts commit e891cc9417055f2214639fb5c71be2318fcd9104.

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -2,7 +2,7 @@ package com.develogical.app;
 
 public class QueryProcessor {
     public String process(String query) {
-        if (query.toLowerCase().contains("shakespeare333")) {
+        if (query.toLowerCase().contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English bard, playwright, and actor, widely regarded as the one of greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
